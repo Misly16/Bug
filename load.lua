13a0cf88bb5d875fd18ef01bcf803c293312c304
Misly <60405462+Misly16@users.noreply.github.com>
2021-11-04 21:31:29 +0000

    load the new grid

--- load.lua
+++ load.lua
@@ -1,9 +1,9 @@
 function love.load()
-    background = love.graphics.newImage('img/grid.jpg')
+    background = love.graphics.newImage('img/grid.png')
     bug_up = love.graphics.newImage('img/bug_up.png')
     bug_down = love.graphics.newImage('img/bug_down.png')
     bug_left = love.graphics.newImage('img/bug_left.png')
     bug_right = love.graphics.newImage('img/bug_right.png')
     x = 0
-    y = 0 
+    y = 0
 end
