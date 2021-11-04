1e36727a04980917fe7fb8a483085875e8ed4431
Misly <60405462+Misly16@users.noreply.github.com>
2021-11-04 21:31:37 +0000

    world border

--- update.lua
+++ update.lua
@@ -3,16 +3,32 @@ function love.update(dt)
     height = love.graphics.getHeight()
 
     if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
-        direction = 'right'
-        map_horizontal = map_horizontal - 100 * dt
+        if map_horizontal <= -1500 then
+            map_horizontal = map_horizontal
+        else
+            direction = 'right'
+            map_horizontal = map_horizontal - 100 * dt
+        end
     elseif love.keyboard.isDown('left') or love.keyboard.isDown('a') then
-        direction = 'left'
-        map_horizontal = map_horizontal + 100 * dt
+        if map_horizontal >= 1500 then
+            map_horizontal = map_horizontal
+        else
+            direction = 'left'
+            map_horizontal = map_horizontal + 100 * dt
+        end
     elseif love.keyboard.isDown('down') or love.keyboard.isDown('s') then
-        direction = 'down'
-        map_vertical = map_vertical - 100 * dt
+        if map_vertical <= -1500 then
+            map_vertical = map_vertical
+        else
+            direction = 'down'
+            map_vertical = map_vertical - 100 * dt
+        end
     elseif love.keyboard.isDown('up') or love.keyboard.isDown('w') then
-        direction = 'up'
-        map_vertical = map_vertical + 100 * dt
+        if map_vertical >= 1500 then
+            map_vertical = map_vertical
+        else
+            direction = 'up'
+            map_vertical = map_vertical + 100 * dt
+        end
     end
 end
